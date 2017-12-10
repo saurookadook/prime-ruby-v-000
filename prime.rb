1ee0a85a3f46bf03626b84c6542419c32e08ff35
Andy Maskiell <maskiella@gmail.com>
2017-12-10 18:15:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,12 @@ def prime?(integer)
   x = 1
   until x > integer
     if ((integer % x) == 0) && !((integer / x) == integer)
-      false
+     return false
     # elsif ((integer % x) == 0) || ((integer / x) > 1)
     #   false
     else
       x += 1
     end
   end
+  return true
 end
